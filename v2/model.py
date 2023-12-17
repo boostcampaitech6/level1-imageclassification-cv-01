278745a1601a69f3d8e56f47be9ba4d245bbf16d
김혜지_T6046 <149589697+carboxaminoo@users.noreply.github.com>
2023-12-17 17:50:39 +0900

    import init,models

--- v2/model.py
+++ v2/model.py
@@ -1,6 +1,9 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
+################
+import torchvision.models as models
+import torch.nn.init as init
+################
 
 class BaseModel(nn.Module):
     """
