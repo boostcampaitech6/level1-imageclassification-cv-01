29e26d7b48d934d99d842ce9f28043fd883800cc
Chloe Han <jh7316@nyu.edu>
2023-12-20 16:09:37 +0500

    feat : switch age label for 59

--- v2/dataset.py
+++ v2/dataset.py
@@ -386,8 +386,12 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
                     mask_label = self._file_names[_file_name]
 
                     id, gender, race, age = profile.split("_")
-                    if(phase=="train" and age in ["57","58","59"]):
+                    # if(phase=="train" and age in ["57","58","59"]):
+                    #     continue
+                    if(phase=="train" and age in ["57","58"]):
                         continue
+                    if(phase=="train" and age in ["59"]):
+                        age="60"
                     gender_label = GenderLabels.from_str(gender)
                     age_label = AgeLabels.from_number(age)
 
