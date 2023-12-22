52a939b3eede771bdaf46eaa418e9d6c035ee358
Chloe Han <jh7316@nyu.edu>
2023-12-22 15:10:15 +0500

    feat : change default resize value in inference

--- v2/inference.py
+++ v2/inference.py
@@ -177,7 +177,7 @@ if __name__ == "__main__":
         "--resize",
         nargs=2,
         type=int,
-        default=(96, 128),
+        default=(236,236),#(96, 128),
         help="resize size for image when you trained (default: (96,128))",
     )
     parser.add_argument(
@@ -198,7 +198,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--model_dir",
         type=str,
-        default=os.environ.get("SM_CHANNEL_MODEL", "../../../models/exp"),
+        default=os.environ.get("SM_CHANNEL_MODEL", "../../../models/exp154"),
     )
     parser.add_argument(
         "--output_dir",
